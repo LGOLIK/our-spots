0dcb667730bf737ba2b9fb1d6f849a04eb968c8e
LGOLIK <golikster@gmail.com>
2016-02-26 14:01:31 -0500

    established home route

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
+var userRoutes = require( path.join(__dirname, '/routes/users'));
+
 var app = express();
 
 app.use(morgan('dev'));
@@ -20,10 +22,10 @@ app.use(methodOverride('_method'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use('/users', userRoutes)
+
 // check that home is up
-app.get('/', (req, res) => {
-  res.send('<h1>Lets go out to eat!</h1>');
-});
+app.get('/', (req, res) => res.render('home'));
 
 // render the register user page
 app.get('/register', (req, res) => res.render('register'));
