ccf19806dc10a3570d5469583a59fa087e0cc0c3
LGOLIK <golikster@gmail.com>
2016-03-02 09:16:16 -0500

    latest seed file

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,10 @@
 INSERT INTO restaurants (name, neighborhood, phone, address, city, state, cuisine, website) VALUES ('The Little Owl', 'West Village', '2127414695', '90 Bedford St', 'New York', 'NY', 'American (New)', 'thelittleowlnyc.com');
 INSERT INTO restaurants (name, neighborhood, phone, address, city, state, cuisine, website) VALUES ('dellanima', 'West Village', '2123666633', '38 8th Ave', 'New York', 'NY', 'Italian', 'www.dellanima.com');
 INSERT INTO restaurants (name, neighborhood, phone, address, city, state, cuisine, website) VALUES ('Sushi of Gari', 'Upper East Side', '2125175340', '402 E 78th St', 'New York', 'NY', 'Japanese', 'www.sushiofgari.com/sog.html');
+INSERT INTO restaurants (name, neighborhood, phone, address, city, state, cuisine, website) VALUES ('Carbone', 'Greenwich Village', '2122543000', '181 Thompson St', 'New York', 'NY', 'Japanese', 'www.sushiofgari.com/sog.html');
+INSERT INTO restaurants (name, neighborhood, phone, address, city, state, cuisine, website) VALUES ('Swine', 'Nolita', '2129937189', '248 Mulberry St', 'New York', 'NY', 'American (New)', 'swinenyc.com');
+INSERT INTO restaurants (name, neighborhood, phone, address, city, state, cuisine, website) VALUES ('Parm', 'Upper East Side', '2125175340', '402 E 78th St', 'New York', 'NY', 'Italian', 'parmnyc.com');
+INSERT INTO restaurants (name, neighborhood, phone, address, city, state, cuisine, website) VALUES ('Salinas', 'Chelsea', '2127761990', '136 9th Ave', 'New York', 'NY', 'Spanish', 'salinasnyc.com');
 
 -- add restaurants to the user
 INSERT INTO rests_users_join VALUES (3, 3, FALSE);
